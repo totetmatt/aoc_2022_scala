670b061c1232f7ae465a6fa3cb0c98168b7ec9ca
totetmatt <matthieu.totet@gmail.com>
2022-12-06 09:08:17 +0100

    day 06 code sizing

--- src/main/scala/Day_06.scala
+++ src/main/scala/Day_06.scala
@@ -1,2 +1,1 @@
-import scala.io.Source
-object Day_06 extends App {Seq(4,14).foreach(x=>println(Source.fromFile("input/day_06/input").mkString.sliding(x).indexWhere(_.toSet.size==x)+x))}
+object Day_06 extends App {Seq(4,14).foreach(x=>println(scala.io.Source.fromFile("input/day_06/input").mkString.sliding(x).indexWhere(_.toSet.size==x)+x))}
